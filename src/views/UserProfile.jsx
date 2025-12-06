aec6d365563be9abac0b3216c41c7d0a93f2e9cc
jg-manalo <jg.manalo@outlook.com>
2025-12-06 18:49:25 +0800

    Added api handling for updating security qna and password

--- src/views/UserProfile.jsx
+++ src/views/UserProfile.jsx
@@ -14,15 +14,20 @@ export default function UserProfile() {
         name: '',
         username: '',
         email: '',
-        password: '',
-        password_confirmation: ''
     });
-
+    const [password, setPassword] = useState({
+        current_password : '',
+        new_password : '',
+        new_password_confirmation : ''
+    });
     const [error, setError] = useState(null);
     const [confirmDelete, setConfirmDelete] = useState(false);
     const [securityQuestion, setSecurityQuestion] = useState([]);
-    const [selectedQuestion, setSelectedQuestion] = useState("");
-    const [newSecurityAnswer, setNewSecurityAnswer] = useState("");
+    const [security, setSecurity] = useState({
+        reset_password_config_id : null,
+        reset_password_config_answer : ""
+    });
+ 
 
     
     useEffect(() => {
@@ -95,14 +100,103 @@ export default function UserProfile() {
             }));
 
             setError(null);
-            toast.success("Updated personal information successfully.", {id : requestToast});
-            
-            setTimeout(() => {
-                logout(); 
-            }, 2000);
-            logout();
+
+            if(data.message === "Nothing to update."){
+                toast.success(data.message, {id: requestToast});
+            } else{
+                toast.success("Updated personal information successfully.", {id : requestToast}); 
+                setTimeout(() => {
+                    logout(); 
+                }, 2000);
+            }
+
         }catch(error){
-            toast.error("Error updating user data.", {id : requestToast});
+            toast.error("Error updating new user data.", {id : requestToast});
+            setError(error);
+        }
+    }
+    const handleUpdateSecurityQna = async (e) => {
+        e.preventDefault();
+        const requestToast = toast.loading("Processing Request..."); 
+        
+        try{ 
+            const res = await fetch(`/api/user/update-security-qna/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept' : 'application/json',
+                    Authorization : `Bearer ${token}`
+                },
+                body: JSON.stringify(security)
+            });
+
+
+            const data = await res.json();
+            console.log(data);
+            if (!res.ok) {
+               const errorData = data.errors;
+               throw errorData;
+            }
+            
+            setUser(prev => ({
+                ...prev,
+                ...data,
+            }));
+
+            setError(null);
+
+            if(data.message === "Nothing to update."){
+                toast.success(data.message, {id: requestToast});
+            } else{
+                toast.success("Updated personal information successfully.", {id : requestToast});
+            }
+
+        }catch(error){
+            toast.error("Error updating new user data.", {id : requestToast});
+            setError(error);
+        }
+    }
+    const handleUpdatePassword = async (e) => {
+        e.preventDefault();
+        const requestToast = toast.loading("Processing Request..."); 
+        
+        try{ 
+            const res = await fetch(`/api/user/update-password/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept' : 'application/json',
+                    Authorization : `Bearer ${token}`
+                },
+                body: JSON.stringify(password)
+            });
+
+
+            const data = await res.json();
+            console.log(data);
+            if (!res.ok) {
+               const errorData = data.errors;
+               throw errorData;
+            }
+            
+            setUser(prev => ({
+                ...prev,
+                ...data,
+            }));
+
+            setError(null);
+
+            if(data.message === "Nothing to update."){
+                toast.success(data.message, {id: requestToast});
+            } else{
+                toast.success("Updated personal information successfully.", {id : requestToast});
+                setTimeout(() => {
+                    logout(); 
+                }, 2000);  
+            }
+
+        }catch(error){
+            toast.error("Error updating password.", {id : requestToast});
             setError(error);
         }
     }
@@ -152,17 +246,17 @@ export default function UserProfile() {
                         </form>
                         
                         {/* --- CARD 2: SECURITY QUESTION --- */}
-                        <form action="" className="flex flex-col h-full w-full">
+                        <form onSubmit={handleUpdateSecurityQna} className="flex flex-col h-full w-full">
                             <div className="bg-radial from-orange-100 via-amber-200/90 via-10% to-orange-300/80 rounded-2xl flex flex-col h-full justify-center p-6 md:p-8 gap-y-4 shadow-xl border border-amber-900/20">
                                 <h1 className='emphasis-text text-2xl text-amber-950 mb-2'>Update Security Question</h1>
                                 <p className='font-serif text-amber-950'>Whenever you feel your security is compromised, it is better to change your security question.</p>
                                 <div className="flex flex-col">
                                     <label htmlFor="security-question" className="font-sans font-semibold text-amber-900 mb-1">New Security Question</label>  
-                                    <select id="security-question" required className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none" value={selectedQuestion} onChange={ e =>{ 
+                                    <select id="security-question" required className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none" value={security.reset_password_config_id ?? ""} onChange={ e =>{ 
                                         const val = e.target.value;
                                         const numValue = val? Number(val) : ""; 
-                                        setUser({...user, reset_password_config_id : numValue});
-                                        setSelectedQuestion(numValue)}}>
+                                        setSecurity({...security, reset_password_config_id : numValue});
+                                        }}>
                                         <option disabled hidden value="" >Select a security question...</option>
                                         {securityQuestion.map((question) => (
                                             <option className="bg-white text-stone-900" key={question.id} value={question.id}>{formatString(question.config)} </option>
@@ -172,7 +266,7 @@ export default function UserProfile() {
 
                                 <div className="flex flex-col">
                                     <label htmlFor="security-answer" className="font-sans font-semibold text-amber-900 mb-1">New Security Answer</label>  
-                                    <input id="security-answer" value={newSecurityAnswer ?? ''} onChange={e => setUser({...user, reset_password_config_answer : e.target.value.toUpperCase()})} required type="text" className="font-sans uppercase bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
+                                    <input id="security-answer" value={security.reset_password_config_answer ?? ''} onChange={e => setSecurity({...security, reset_password_config_answer : e.target.value.toUpperCase()})} required type="text" className="font-sans uppercase bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
                                 </div>
 
                                 <button className="mt-4 mx-auto cursor-pointer bg-gradient-to-b from-emerald-500 to-teal-800 hover:to-teal-700 text-white shadow-md hover:shadow-emerald-400/50 hover:scale-105 transition-all duration-200 border-2 border-green-950 px-8 py-3 rounded-xl font-bold tracking-wide">SAVE CHANGES</button>
@@ -180,28 +274,31 @@ export default function UserProfile() {
                         </form>
 
                         {/* --- CARD 3: PASSWORD (Fixed Security Logic) --- */}
-                        <form action="" className="flex flex-col h-full w-full">
+                        <form onSubmit={handleUpdatePassword} className="flex flex-col h-full w-full">
                             <div className="bg-radial from-orange-100 via-amber-200/90 via-10% to-orange-300/80 rounded-2xl flex flex-col h-full justify-center p-6 md:p-8 gap-y-4 shadow-xl border border-amber-900/20">
                                 <h1 className='emphasis-text text-2xl text-amber-950 mb-2'>Update Password</h1>
                                 
                                 {/* ADDED: Current Password Field */}
                                 <div className="flex flex-col items-start">
                                     <label htmlFor="current-password" className="font-sans font-semibold text-amber-900 mb-1">Current Password</label>  
-                                    <input id="current-password" required type="password" placeholder="Verify it's you..." className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
+                                    <input id="current-password" required type="password" value={password.current_password?? ''} onChange={e => setPassword({...password, current_password : e.target.value})} placeholder="Verify it's you..." className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
+                                    {error && <p className="font-bold text-rose-700 text-sm mt-1">{error}</p>}    
                                 </div>
                                 
                                 <hr className="border-amber-900/20 my-2"/>
 
                                 <div className="flex flex-col items-start">
                                     <label htmlFor="new-password" className="font-sans font-semibold text-amber-900 mb-1">New Password</label>  
-                                    <input id="new-password" required type="password" className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
-                                    {error && <p className="font-bold text-rose-700 text-sm mt-1">{error.username}</p>}
+                                    <input id="new-password" required type="password" value={password.new_password?? ''} onChange={e => setPassword({...password, new_password : e.target.value})} className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
+                                    {error && <p className="font-bold text-rose-700 text-sm mt-1">{error.new_password}</p>}    
                                 </div>
 
                                 <div className="flex flex-col items-start">
                                     <label htmlFor="confirm-password" className="font-sans font-semibold text-amber-900 mb-1">Confirm New Password</label>  
-                                    <input id="confirm-password" required type="password" className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
-                                    {error && <p className="font-bold text-rose-700 text-sm mt-1">{error.email}</p>}
+                                    <input id="confirm-password" required type="password" value={password.new_password_confirmation?? ''} onChange={e => setPassword({...password, new_password_confirmation : e.target.value})} className="font-sans bg-stone-50 border border-stone-300 text-stone-900 rounded-lg w-full p-3 focus:ring-2 focus:ring-amber-500 outline-none"/>
+                                    {error && <p className="font-bold text-rose-700 text-sm mt-1">{error.new_password}</p>}    
+                                    
+                                    {/* {error && <p className="font-bold text-rose-700 text-sm mt-1">{error.email}</p>} */}
                                 </div>
 
                                 <button className="mt-4 mx-auto cursor-pointer bg-gradient-to-b from-emerald-500 to-teal-800 hover:to-teal-700 text-white shadow-md hover:shadow-emerald-400/50 hover:scale-105 transition-all duration-200 border-2 border-green-950 px-8 py-3 rounded-xl font-bold tracking-wide">UPDATE PASSWORD</button>
@@ -242,7 +339,5 @@ export default function UserProfile() {
             </MainLayout>             
         </div>
     </>
-    
-  
   );
 }
