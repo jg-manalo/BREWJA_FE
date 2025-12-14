fba70f00c85ee07972b632f1a0e1b2b5488c4cff
jg-manalo <jg.manalo@outlook.com>
2025-12-14 16:10:48 +0800

    Refine the looks of the card of the create brew.
    added design on the leaf type modal

--- src/views/CreateBrew.jsx
+++ src/views/CreateBrew.jsx
@@ -5,6 +5,8 @@ import toast from 'react-hot-toast';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import defaultImage from '../assets/tea.png';
+import LeafBadge from "../components/LeafBadgeButton";
+import { leafTypeColors } from "../constants/LeafTypeColors";
 
 export default function CreateBrew(){
     const [showLeafOption, setShowLeafOption] = useState(false);
@@ -17,7 +19,7 @@ export default function CreateBrew(){
         leaf_type: null,
         minutes: null,
         seconds: null,
-        sweetener: 'None',
+        sweetener: '',
         tsp_sweetener : null,
         fluid_oz : null,
         is_private : false,
@@ -92,50 +94,53 @@ export default function CreateBrew(){
 
         setBrew({...brew, image : file, imagePreview : URL.createObjectURL(file)});
     }
+
+
     return (
         <>
-            <div className="update-profile-bg">
+            <div className="create-brews-page-bg">
                 <MainLayout>
-                    <main className="flex flex-col min-h-screen justify-center pt-8 pb-8 w-full">
+                    <main className="flex flex-col min-h-screen pt-8 pb-8 w-full font-serif">
                         <AppToaster />
-                        <form onSubmit={handleSubmitBrew} className='flex flex-row gap-x-2 w-full  justify-center'>
-                            <div className="flex flex-col min-h-[25%]  p-4 gap-y-4 bg-amber-300">
-                                <div className="flex flex-col items-end">
-                                    <button type="button" onClick={() => setShowLeafOption(true)} className="cursor-pointer">{brew.leaf_type 
+                        <form onSubmit={handleSubmitBrew} className='flex gap-y-4 lg:max-w-full lg:w-full  justify-center '>
+                            <div className="flex flex-col rounded-lg p-4 gap-y-4 bg-linear-45 from-indigo-100 to-red-100 shadow-xl lg:w-[50%]  h-fit">
+                                <div className="flex flex-row items-start">
+                                    <div className="flex flex-grow">
+                                        <label className="inline-flex items-center cursor-pointer">
+                                            <input type="checkbox" checked={brew.is_private || false} onChange={() => setBrew({...brew, is_private : !brew.is_private})} className="sr-only peer"/>
+                                            <div className="bg-indigo-300 relative w-9 h-5 bg-neutral-quaternary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
+                                            <span className="select-none ms-3 text-sm font-medium text-heading">{brew.is_private? `Private` : `Public`}</span>
+                                        </label>
+                                    </div>
+                                    <LeafBadge typeId={brew.leaf_type} leafList={leafType} onEdit={() => setShowLeafOption(true)}>
+                                        {brew.leaf_type 
                                         ? leafType.find(l => l.id == brew.leaf_type)?.type 
-                                        : `Leaf Type`
-                                    }</button>
+                                        : `Select Leaf Type`}                                     
+                                    </LeafBadge>
                                     {error && (<p className="text-red-500 lg:text-xs font-serif">{error.leaf_type}</p>)}
                                 </div>
-                                <div className="flex flex-col items-center gap-y-4">
-                                    <div className="relative lg:w-[326px] lg:h-[326px] w-[128px] h-[128px] group">
+                                <div className="flex flex-col items-center gap-y-4 w-full">
+                                    <div className="relative lg:w-[454px] lg:h-[326px] w-[128px] h-[128px] group">
                                         <input type="file" accept="image/jpg, image/jpeg, image/png" onChange={handleUploadImage}  className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-20"/>
                                         <img src={brew.imagePreview?? defaultImage} alt="" className="w-full h-full object-cover" />
                                         <div className="absolute inset-0 bg-gray-800/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center  z-10">
                                             <span className="text-white text-sm font-medium">Upload Photo</span>
                                         </div>
                                     </div>
-                                    
-                                    <input required type="text" placeholder="Brew Title" value={brew.title?? ''} onChange={e => setBrew({...brew, title : e.target.value})}/>
-                                    <textarea maxLength={255} placeholder="description" id="" value={brew.description} onChange={e => setBrew({...brew, description : e.target.value})} className="border-2"></textarea>
-                                    <label className="inline-flex items-center cursor-pointer">
-                                        <input type="checkbox" checked={brew.is_private || false} onChange={() => setBrew({...brew, is_private : !brew.is_private})} className="sr-only peer"/>
-                                        <div className="relative w-9 h-5 bg-neutral-quaternary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
-                                        <span className="select-none ms-3 text-sm font-medium text-heading">{brew.is_private? `Private` : `Public`}</span>
-                                    </label>
+                                    <input required type="text" className="text-center w-full text-4xl emphasis-text" placeholder="Brew Title" value={brew.title?? ''} onChange={e => setBrew({...brew, title : e.target.value})}/>
+                                    <textarea className="w-full text-justify" maxLength={255} placeholder="description" id="" value={brew.description} onChange={e => setBrew({...brew, description : e.target.value})}></textarea>
                                 </div>
-                            </div>
-                            <div className="flex flex-col min-h-[50%] p-4 gap-y-4 bg-amber-300">
-                               <div className="flex justify-center">
-                                    <h1>Steeping Info</h1>
+                                <hr />
+                                <div className="flex justify-center">
+                                    <h1 className="font-bold complementary-emphasis text-2xl">Steeping Info</h1>
                                </div>
                                 <div className="flex gap-x-2">
-                                    <label htmlFor="fluid-oz">Fluid oz</label>
-                                    <input required type="number" className="flex flex-col"min={0} id="fluid-oz" value={brew.fluid_oz?? ''} onChange={e => setBrew({...brew, fluid_oz : e.target.valueAsNumber})}/>
+                                    <label htmlFor="fluid-oz">Fluid oz:</label>
+                                    <input required type="number" className="flex flex-col" min={0} placeholder="8 (oz)" id="fluid-oz" value={brew.fluid_oz?? ''} onChange={e => setBrew({...brew, fluid_oz : e.target.valueAsNumber})}/>
                                 </div>
                                 {error && (<p className="text-red-500 lg:text-xs font-serif">{error.fluid_oz}</p>)}
                                 <div className="flex items-start gap-x-2">
-                                    <label htmlFor="sweetener">Sweetener</label>
+                                    <label htmlFor="sweetener">Sweetener:</label>
                                     <select name="" id="sweetener" value={brew.sweetener?? 'None'} onChange={e => setBrew({...brew, sweetener : e.target.value})}>
                                         <option value="None">None</option>
                                         <option value="Honey">Honey</option>
@@ -144,29 +149,58 @@ export default function CreateBrew(){
                                 </div>
                                 {brew.sweetener !== 'None' && (
                                     <div className="flex items-start gap-x-2 ">
-                                    
-                                        <label htmlFor="tsp-sweetener">tsp</label>
-                                        <input type="number" className="flex flex-col"min={0} placeholder="1 tsp" id="tsp-sweetener" value={brew.tsp_sweetener} onChange={e => setBrew({...brew, tsp_sweetener : e.target.valueAsNumber})}/>
+                                        <label htmlFor="tsp-sweetener">tsp:</label>
+                                        <input type="number" className="flex flex-col"min={0} placeholder="1 tsp" id="tsp-sweetener" value={brew.tsp_sweetener?? undefined} onChange={e => setBrew({...brew, tsp_sweetener : e.target.valueAsNumber})}/>
                                     </div>
                                 )}
-                                <div className="flex items-start gap-x-2 justify-center ">
-                                    <label htmlFor="duration">Duration</label>
-                                    <input required placeholder="minutes" type="number" className="flex flex-col"min={0} step={1} id="duration" value={brew.minutes?? ''} onChange={e => setBrew({...brew, minutes : parseInt(e.target.value)})}/>
-                                    <input required placeholder="seconds" type="number" className="flex flex-col"min={0} step={1} id="duration-s" value={brew.seconds?? ''} onChange={e => setBrew({...brew, seconds : parseInt(e.target.value)})}/>
+                               <div className="flex items-center justify-start gap-2 flex-wrap">
+                                    <label htmlFor="minute" className="whitespace-nowrap font-medium">Minute/s:</label>
+                                    
+                                    {/* Minutes Input */}
+                                    <input 
+                                        required 
+                                        placeholder="mins" 
+                                        type="number" 
+                                        min={0} 
+                                        step={1} 
+                                        id="minute" 
+                                        value={brew.minutes ?? ''} 
+                                        onChange={e => setBrew({ ...brew, minutes: parseInt(e.target.value) })}
+                                        className="w-20 p-1 rounded border border-gray-400 text-center" 
+                                    />
+                                    <label htmlFor="seconds" className="whitespace-nowrap font-medium">Second/s:</label>
+
+                                    {/* Seconds Input */}
+                                    <input 
+                                        required 
+                                        placeholder="secs" 
+                                        type="number" 
+                                        min={0} 
+                                        step={1} 
+                                        id="seconds" 
+                                        value={brew.seconds ?? ''} 
+                                        onChange={e => setBrew({ ...brew, seconds: parseInt(e.target.value) })}
+                                        className="w-20 p-1 rounded border border-gray-400 text-center" 
+                                    />
+                                    
                                 </div>
                                 {error && (<p className="text-red-500 lg:text-xs font-serif">{error.steep_duration}</p>)}
                                 <button className="mt-4 mx-auto cursor-pointer bg-gradient-to-b from-emerald-500 to-teal-800 hover:to-teal-700 text-white shadow-md hover:shadow-emerald-400/50 hover:scale-105 transition-all duration-200 border-2 border-green-950 px-8 py-3 rounded-xl font-bold tracking-wide">Save</button>
                             </div>
                             {showLeafOption && (
                                 <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black/20 backdrop-blur-sm">
-                                    <div className="bg-gradient-to-br from-orange-100 via-amber-200 to-orange-300 rounded-2xl flex flex-col lg:max-w-xl w-[90%] justify-center p-8 gap-y-6 shadow-2xl border-2 border-amber-500">
-                                        <button type="button" onClick={() => setShowLeafOption(false)}>Close</button>
-                                        <h1>Leaf Type</h1>
-                                        <div>
+                                    <div className="bg-linear-45 from-indigo-100 to-red-100 rounded-2xl flex flex-col lg:max-w-xl w-[90%] justify-center h-fit p-8 gap-y-6 shadow-2xl border-2 border-red-900">
+                                        <div className="flex flex-col gap-y-1">    
+                                            <div className="flex justify-end">
+                                                <button type="button" onClick={() => setShowLeafOption(false)} className="text-amber-900 hover:text-amber-700 font-bold px-2 cursor-pointer">✕</button>
+                                            </div>
+                                            <h1 className="emphasis-text text-4xl m-auto">Leaf Type</h1>
+                                        </div>
+                                        <div className="grid grid-cols-2 gap-x-2 gap-y-2 items-center">
                                             {leafType.map((leaf) => (
-                                                <div key={leaf.id}>
+                                                <div key={leaf.id} className={`${leafTypeColors[leaf.id]} rounded-full flex items-center gap-3 px-4 py-2`}>
+                                                    <input type="radio" required name="leaf_selection" value={leaf.id} checked={brew.leaf_type == leaf.id} onChange={e => setBrew({...brew, leaf_type : e.target.value})} id={`leaf-${leaf.id}`} className="cursor-pointer"/> 
                                                     <label htmlFor={`leaf-${leaf.id}`}>{leaf.type}</label> 
-                                                    <input type="radio" required name="leaf_selection" value={leaf.id} checked={brew.leaf_type == leaf.id} onChange={e => setBrew({...brew, leaf_type : e.target.value})} id={`leaf-${leaf.id}`}/> 
                                                 </div>
                                             ))}
                                         </div>
